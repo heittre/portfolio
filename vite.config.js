b4ce0e885d5ddecd0ada240c6683ceb77972f4f0
Chamudi Siriwardhane <116910122+heittre@users.noreply.github.com>
2024-08-19 12:28:06 +0530

    minor settings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from 'tailwindcss';
+// import autoprefixer from 'autoprefixer';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+  base: '/portfolio/', // Add this line
 })
