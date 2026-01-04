21f8dc29f8771f06038f5b809ca6ac9ab3b8f0a2
Chamudi Siriwardhane <116910122+heittre@users.noreply.github.com>
2026-01-04 16:04:15 +0530

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home";
 import FullStack from "./Pages/FullStack";
 import TestAutomation from "./Pages/TestAutomation"
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from 'tailwindcss';
-// import autoprefixer from 'autoprefixer';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-  
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
