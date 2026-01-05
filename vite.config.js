9ec67d754d149b77eb9c1da9cd08e9f6ea27e1db
Chamudi Siriwardhane <116910122+heittre@users.noreply.github.com>
2026-01-05 19:03:04 +0530

    add hashrouter

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
-  base: '/portfolio/', // Add this line
+  plugins: [react()],
+  base: '/portfolio/' // important for GitHub Pages
 })
